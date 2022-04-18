88d99faf03ffa53ebe8a07917a5d09a2dcd6cdc9
Monika Repčytė <96413760+mrepcyte@users.noreply.github.com>
2022-04-18 21:11:13 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {GlobalStyle} from './styles'
+import Header from './components/header'
+
+const App = () => {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Header />
+    </div>
+  )
+}
+
+export default App
